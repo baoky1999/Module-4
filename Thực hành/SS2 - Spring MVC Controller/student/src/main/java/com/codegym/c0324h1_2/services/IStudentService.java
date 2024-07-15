4b81c782c750f5509e5448d034dc275ed2e5baa4
baoky1999 <kinataru0123@gmail.com>
2024-07-15 13:54:00 +0700

    update student

---
+++ Thực hành/SS2 - Spring MVC Controller/student/src/main/java/com/codegym/c0324h1_2/services/IStudentService.java
@@ -0,0 +1,15 @@
+package com.codegym.c0324h1_2.services;
+
+import com.codegym.c0324h1_2.models.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+    List<Student> findAll();
+
+    void save(Student student);
+
+    Student findById(Long id);
+
+    void update(Student student);
+}
