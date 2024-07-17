f1cd277ded57239957344ccc6ce284d69197c712
baoky1999 <kinataru0123@gmail.com>
2024-07-17 09:23:21 +0700

    thymeleaf

---
+++ Thực hành/SS3 - View & Thymeleaf/customer-management-thymeleaf/src/main/java/com/customer/customermanagementthymeleaf/service/ICustomerService.java
@@ -0,0 +1,18 @@
+package com.customer.customermanagementthymeleaf.service;
+
+import com.customer.customermanagementthymeleaf.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
