60a05b82a22f2c55693552d591ba23f0b464cb41
baoky1999 <kinataru0123@gmail.com>
2024-07-23 08:59:27 +0700

    validation

--- Bai_tap_Spring_boot/SS7_Spring_Data_JPA/blog-update/src/main/java/com/blog/blogupdate/repository/ICategoryRepository.java
+++ Bai_tap_Spring_boot/SS7_Spring_Data_JPA/blog-update/src/main/java/com/blog/blogupdate/repository/ICategoryRepository.java
@@ -3,5 +3,5 @@ package com.blog.blogupdate.repository;
 import com.blog.blogupdate.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
 }
