46d100ca6f5b937a033d2e2bcb68d57b69e0e06f
baoky1999 <kinataru0123@gmail.com>
2024-07-29 08:45:05 +0700

    RESTful

--- Bai_tap_Spring_boot/Webservice & RESTful API/blog-update/src/main/java/com/blog/blogupdate/controller/RestCategoryController.java
+++ Bai_tap_Spring_boot/Webservice & RESTful API/blog-update/src/main/java/com/blog/blogupdate/controller/RestCategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/category")
+@RequestMapping("/api/categorys")
 public class RestCategoryController {
 
     @Autowired
