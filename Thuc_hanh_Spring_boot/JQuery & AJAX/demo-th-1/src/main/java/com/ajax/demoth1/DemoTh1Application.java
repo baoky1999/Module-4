dc5b0f66ca06f4fd051ad13b6cb0b7e17c9f3eb5
baoky1999 <kinataru0123@gmail.com>
2024-07-29 08:08:19 +0700

    jquey

---
+++ Thuc_hanh_Spring_boot/JQuery & AJAX/demo-th-1/src/main/java/com/ajax/demoth1/DemoTh1Application.java
@@ -0,0 +1,13 @@
+package com.ajax.demoth1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTh1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTh1Application.class, args);
+    }
+
+}
---
+++ Thuc_hanh_Spring_boot/JQuery & AJAX/demo-th-1/src/test/java/com/ajax/demoth1/DemoTh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajax.demoth1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
