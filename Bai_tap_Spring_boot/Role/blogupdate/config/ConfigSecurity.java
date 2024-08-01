b79ac4ee270621abba6fe9af85c76f89ea4a0e21
baoky1999 <kinataru0123@gmail.com>
2024-08-01 15:15:28 +0700

    role

---
+++ Bai_tap_Spring_boot/Role/blogupdate/config/ConfigSecurity.java
@@ -0,0 +1,68 @@
+package com.blog.blogupdate.config;
+
+import com.blog.blogupdate.service.iml.UserInforDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class ConfigSecurity {
+
+    @Autowired
+    private UserInforDetailService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable())
+//                Config các đường dẫn không cần đăng nhập
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers("/blog", "/login","/403", "/logoutSuccessful", "blog/{id}/view").permitAll())
+//                Config các đường dẫn bắt buộc cần đăng nhập
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers("/blog/create", "/logout", "/blog/{id}/delete", "/blog/{id}/update").authenticated())
+//               Cấu hình lại form login
+                .formLogin((formLogin) ->
+                        formLogin
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .loginPage("/login")
+                                .failureUrl("/login?error=true")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/blog"))
+                .logout((logout) ->
+                        logout.deleteCookies("remove")
+                                .invalidateHttpSession(false)
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/logoutSuccessful"))
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .accessDeniedPage("/403"));
+        return http.build();
+    }
+}
